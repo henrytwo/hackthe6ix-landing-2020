97db97088ab244f2cbd2a0238c6c3bb64d28e726
Henry T <henry@henrytu.me>
2020-07-12 14:13:52 -0400

    Match shadow and disclaimer

--- src/data/Disclaimer.vue
+++ src/data/Disclaimer.vue
@@ -1,5 +1,5 @@
 <template>
   <p>
-    This is an <b>important</b> disclaimer!
+    Hack the 6ix is going <b>digital!</b> 🎉 Join us from anywhere across the world, applications are open until July 24 at 11:59 EST.
   </p>
 </template>
