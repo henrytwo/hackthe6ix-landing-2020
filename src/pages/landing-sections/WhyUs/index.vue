be590e3886c9e2b11b5ff2c3747c862b1cc67186
Henry T <henry@henrytu.me>
2020-07-13 16:01:07 -0400

    Starting why-us components

--- src/pages/landing-sections/WhyUs/index.vue
+++ src/pages/landing-sections/WhyUs/index.vue
@@ -5,46 +5,40 @@
       <!-- Should make a component here -->
 
       <!-- Meet meet new friends -->
-      <div :class="$style.whyus__group">
-        <div :class="$style['whyus__leftimg--left']">
-          <h2>MEET NEW FRIENDS</h2>
-          <p>
-            Physical distancing is serious, but meeting new people doesn't have to stop.
+      <Reason
+          title="MEET NEW FRIENDS"
+          text="Physical distancing is serious, but meeting new people doesn't have to stop.
             Get geared up in your pajamas and chat away behind the safety of your digital
-            screen. The only things we'll spread are heart reacts and lols.
-          </p>
-        </div>
-        <div :class="$style['whyus__rightimg--right']">
-          <img :class="$style.whyus__discord" src="@assets/04_a_Why_Us_Meet_People/GuyDiscord.svg">
-        </div>
-      </div>
+            screen. The only things we'll spread are heart reacts and lols."
+          image="04_a_Why_Us_Meet_People/GuyDiscord.svg"
+          image-x="right"
+          />
 
       <!-- Find a team -->
-      <div :class="$style.whyus__group">
-        <div :class="$style['whyus__leftimg--left']">
-          <img src="@assets/04_a_Why_Us_Meet_People/Container_Find_A_Team.svg">
-        </div>
-        <div :class="$style['whyus__leftimg--right']">
-          <h2>FIND A TEAM</h2>
-          <p>
-            The most memorable part of a hackathon are the people we work with. Create
+      <Reason
+          title="FIND A TEAM"
+          text="The most memorable part of a hackathon are the people we work with. Create
             your own team or find new people to work with. Don't know who to work with?
-            We can help with that.
-          </p>
-        </div>
-      </div>
+            We can help with that."
+          image="04_a_Why_Us_Meet_People/Container_Find_A_Team.svg"
+          image-x="left"
+          vjustify="bottom"
+      />
+
     </div>
   </Container>
 </template>
 
 <script>
 import {Container} from '@components';
+import Reason from './Reason/index';
 import Button from '@hackthe6ix/vue-ui/Button';
 
 export default {
   components: {
     Container,
-    Button
+    Button,
+    Reason
   }
 }
 </script>
