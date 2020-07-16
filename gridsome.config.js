458a26fe65a0c4e26ca7432e639a68cae0c42069
fpunny <me@fpunny.xyz>
2020-07-16 01:50:34 -0400

    Added new CMS stuff

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,7 +4,8 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-const {resolve} = require('path');
+const { resolve, basename } = require('path');
+require('dotenv').config();
 
 module.exports = {
   configureWebpack: {
@@ -22,11 +23,13 @@ module.exports = {
     },
   },
   siteName: 'Hack The 6ix',
+  siteDescription: 'Hack the 6ix is the largest summer student run, not for profit hackathon located in the heart of Toronto. Every year, we select 400+ participants from a pool of over 1500 remarkable developers, designers, and innovators. We believe the diversity and talent of our hackers contribute greatly to the success of our event. Now in its sixth iteration, Hack the 6ix has established itself as a key player in the Toronto tech ecosystem and provides a platform for students to present their ideas of the future.',
   plugins: [
     {
-      use: `gridsome-plugin-netlify-cms`,
+      use: '@gridsome/source-contentful',
       options: {
-        publicPath: `/admin`
+        space: process.env.CONTENTFUL_SPACE,
+        accessToken: process.env.CONTENTFUL_TOKEN,
       }
     },
   ],
