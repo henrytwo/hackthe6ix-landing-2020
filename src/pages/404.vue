ebe579d7548af03298817c71e7061b02c39f406b
Henry T <henry@henrytu.me>
2020-07-15 22:40:25 -0400

    404 error page

---
+++ src/pages/404.vue
@@ -0,0 +1,32 @@
+<template>
+  <Container :block="$style.error404" as="section">
+    <h1>
+      Error 404
+    </h1>
+    <h3>
+      Sorry, we couldn't find that resource :(
+    </h3>
+    <Button v-on:click.native="home()">
+      Go back home
+    </Button>
+  </Container>
+</template>
+
+<script>
+  import {Container} from '@components';
+  import Button from "@hackthe6ix/vue-ui/Button/index";
+
+  export default {
+    components: {
+      Button,
+      Container
+    },
+    methods: {
+      home() {
+        document.location.href = '/';
+      }
+    }
+  }
+</script>
+
+<style src="./Index.module.scss" lang="scss" module />
