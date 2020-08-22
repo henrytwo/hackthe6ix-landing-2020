34a23d2f1e2cbac3b1b73a2ccb6fc0044a77733b
Henry Tu <henry@henrytu.me>
2020-08-22 01:56:46 -0400

    Remove Safari warning

--- src/landing-sections/Schedule/index.vue
+++ src/landing-sections/Schedule/index.vue
@@ -17,7 +17,6 @@
         <TextComponent type="heading4" as="span">{{ item.node.label }}</TextComponent>
       </li>
     </Stack>
-    <TextComponent type="heading4" as="span">Note: We are currently investigating issues with the schedule on Safari and iOS devices. Opening ceremonies will begin at 6pm EST.</TextComponent><br><br>
     <div :class="$style.window">
       <div :class="$style.bar">
         <div :class="$style.dots">
